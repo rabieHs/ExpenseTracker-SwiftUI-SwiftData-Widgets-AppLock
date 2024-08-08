2b416b470df602a05ee527764cdb8db34120f20c
rabieHs <rabiehoussaini@gmail.com>
2024-08-08 13:45:46 +0100

    intergate swift data and complete the crud

---
+++ ExpenseTracker/Views/NewExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  NewExpenseView.swift
+//  ExpenseTracker
+//
+//  Created by rabie houssaini on 8/8/2024.
+//
+
+import SwiftUI
+
+struct NewExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewExpenseView()
+}
